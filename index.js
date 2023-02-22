aad7e3c6c2515f0a9dd08e0112d3d451e4d5d85d
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-02-22 14:08:32 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 require('dotenv').config();
 const PORT = process.env.PORT || 8081;
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(express.json());
 
-
-
 app.get("/", (req, res) => {
     res.send("Welcome to Auto response Application");
 });
+
+app.
+
 app.listen(PORT,() => {
     console.log(`Server listening on port ${PORT}`);
 })
